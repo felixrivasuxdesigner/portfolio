1e914502667d819005fe0bea22aa22244c076291
Felix Rivas <rivafelix@gmail.com>
2025-05-06 13:14:07 -0400

    Actualiza color de acento a #FC6736 en SASS y site.webmanifest

--- .eleventy.js
+++ .eleventy.js
@@ -12,6 +12,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/assets/resumen');
   eleventyConfig.addPassthroughCopy('src/robots.txt');
   eleventyConfig.addPassthroughCopy('src/sitemap.xml');
+  eleventyConfig.addPassthroughCopy('src/site.webmanifest');
 
   // Watch for changes - no observamos la carpeta CSS para evitar el bucle infinito
   eleventyConfig.addWatchTarget('src/assets/scss/');
