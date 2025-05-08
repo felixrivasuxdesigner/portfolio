b47963da579e1994588bf85d18d8eed542eba218
Felix Rivas <rivafelix@gmail.com>
2025-05-08 17:16:21 -0400

    the last chance

--- src/_data/translations.js
+++ src/_data/translations.js
@@ -3,18 +3,13 @@ const es = require('./translations/es.json');
 const caseStudiesEn = require('./translations/case-studies/en.json');
 const caseStudiesEs = require('./translations/case-studies/es.json');
 
-// Reorganizar la estructura para garantizar que las traducciones funcionen correctamente
 module.exports = {
   es: {
     ...es,
-    case_studies: {
-      ...caseStudiesEs,
-    },
+    case_studies: caseStudiesEs,
   },
   en: {
     ...en,
-    case_studies: {
-      ...caseStudiesEn,
-    },
+    case_studies: caseStudiesEn,
   },
 };
