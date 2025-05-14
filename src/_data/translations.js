d8442f2c08f7f707d2ffc27facfbe7310a881aee
Felix Rivas <rivafelix@gmail.com>
2025-05-14 00:47:24 -0400

    Enhance JourneyLaw case study with detailed project challenges, roles, and technical solutions; update titles, categories, and metrics for improved clarity and impact in both English and Spanish versions.

--- src/_data/translations.js
+++ src/_data/translations.js
@@ -1,10 +1,14 @@
 module.exports = {
   es: {
     ...require('./translations/es.json'),
-    case_studies: require('./translations/case-studies/es.json')
+    case_studies: {
+      ...require('./translations/case-studies/es.json')
+    }
   },
   en: {
     ...require('./translations/en.json'),
-    case_studies: require('./translations/case-studies/en.json')
+    case_studies: {
+      ...require('./translations/case-studies/en.json')
+    }
   }
 };
